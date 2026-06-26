765570d37969ea24b5f5c82bb318fc8a83d84096
srinivas <srinivas.renee@gmail.com>
2026-06-27 00:09:00 +0530

    pubsubnet

--- main.tf
+++ main.tf
@@ -11,3 +11,19 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_block[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_names[count.index]
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    },
+    var.public_subnet_tags
+
+  )
+}
--- variables.tf
+++ variables.tf
@@ -19,3 +19,17 @@ variable "igw_tags" {
   type = map(string)
 
 }
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+
+}
+variable "public_subnet_tags" {
+
+  type = map(string)
+  default = {
+
+  }
+
+}
