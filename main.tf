a7cd40e3a253200853387d307811b19f95f207a8
srinivas <srinivas.renee@gmail.com>
2026-06-27 12:30:19 +0530

    route

--- main.tf
+++ main.tf
@@ -100,6 +100,12 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+  destination_cidr_block = var.public_cidr
+
+}
 
 /* resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidrs)
@@ -113,9 +119,4 @@ resource "aws_route_table_association" "database" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  gateway_id             = aws_internet_gateway.main.id
-  destination_cidr_block = var.public_cidr
-
-} */
+ */
