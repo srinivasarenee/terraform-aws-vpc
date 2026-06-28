3903c407850c1e7c2f8cae3b02866597e7bde751
srinivas <srinivas.renee@gmail.com>
2026-06-28 12:57:32 +0530

    push

--- output.tf
+++ output.tf
@@ -5,3 +5,18 @@ output "azs_info" {
 output "aws-eip_id" {
   value = aws_eip.nat.id
 }
+
+output "vpc_id" {
+
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
